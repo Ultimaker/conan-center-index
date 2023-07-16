5ae6632d73e5732cfc441c0af233404d7d01caf2
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-07-16 11:14:14 +0200

    Patch resources such that they adhere to CCI conventions
    Contributes to:
    - conan-io/conan-center-index#18561
    - projectchrono/chrono#270

--- recipes/projectchrono/all/test_package/test_package.cpp
+++ recipes/projectchrono/all/test_package/test_package.cpp
@@ -4,11 +4,13 @@
 #include <chrono/ChVersion.h>
 #include <chrono/core/ChLog.h>
 #include <chrono/core/ChVector.h>
-
+#include <chrono/core/ChGlobal.h>
 
 int main(void) {
     std::cout << "Chrono version: " << CHRONO_VERSION << std::endl;
     chrono::GetLog() << "Hello world from ChronoEngine!" << "\n";
-    chrono::ChVector<> mvect2;
+    chrono::GetLog() << "Chrono version: " << CHRONO_VERSION << "\n";
+    chrono::GetLog() << "ChVector: " << chrono::ChVector<>(1, 2, 3) << "\n";
+    chrono::GetLog() << "Data path: " << chrono::GetChronoDataPath() << "\n";
     return EXIT_SUCCESS;
 }
