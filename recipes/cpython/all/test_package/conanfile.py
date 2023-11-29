164618bb513c90b58380023758d2a61111834e0e
Jelle Spijker <jelle.spijker@ultimaker.com>
2023-11-29 13:38:39 +0100

    Quick fix Cura cpython 3.10.4 recipe
    Contributes to CURA-10831

--- recipes/cpython/all/test_package/conanfile.py
+++ recipes/cpython/all/test_package/conanfile.py
@@ -1,91 +1,143 @@
-from io import StringIO
-from pathlib import Path
-
+from conans import AutoToolsBuildEnvironment, CMake, RunEnvironment
+from conans import tools as legacy_tools
 from conan import ConanFile
-from conan.tools.apple import is_apple_os
-from conan.tools.build import can_run
-from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
-from conan.tools.scm import Version
-from conan.tools.env import VirtualBuildEnv, VirtualRunEnv, Environment
-from conan.tools.microsoft import is_msvc
 from conan.errors import ConanException
-from conan.tools.files import chdir
+from conan.tools.build import can_run
+from conan.tools.scm import Version
+from conan.tools.apple import is_apple_os
+from conan.tools.files import mkdir
+from conan.tools.microsoft import is_msvc
+from io import StringIO
+import os
+import re
+import shutil
+
+
+class CmakePython3Abi(object):
+    def __init__(self, debug, pymalloc, unicode):
+        self.debug, self.pymalloc, self.unicode = debug, pymalloc, unicode
+
+    _cmake_lut = {
+        None: "ANY",
+        True: "ON",
+        False: "OFF",
+    }
+
+    @property
+    def suffix(self):
+        return "{}{}{}".format(
+            "d" if self.debug else "",
+            "m" if self.pymalloc else "",
+            "u" if self.unicode else "",
+        )
+
+    @property
+    def cmake_arg(self):
+        return ";".join(self._cmake_lut[a] for a in (self.debug, self.pymalloc, self.unicode))
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "arch", "compiler", "build_type"
-    test_type = "explicit"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    @property
+    def _py_version(self):
+        return re.match(r"^([0-9.]+)", self.deps_cpp_info["cpython"].version).group(1)
+
+    @property
+    def _pymalloc(self):
+        return bool("pymalloc" in self.options["cpython"] and self.options["cpython"].pymalloc)
+
+    @property
+    def _cmake_abi(self):
+        if self._py_version < Version("3.8"):
+            return CmakePython3Abi(
+                debug=self.settings.build_type == "Debug",
+                pymalloc=self._pymalloc,
+                unicode=False,
+            )
+        else:
+            return CmakePython3Abi(
+                debug=self.settings.build_type == "Debug",
+                pymalloc=False,
+                unicode=False,
+            )
+
+    @property
+    def _cmake_try_FindPythonX(self):
+        if is_msvc(self) and self.settings.build_type == "Debug":
+            return False
+        return True
 
     @property
     def _supports_modules(self):
         return not is_msvc(self) or self.options["cpython"].shared
 
-    def _cpython_option(self, name):
-        try:
-            return getattr(self.options["cpython"], name, False)
-        except ConanException:
-            return False
-
-    def requirements(self):
-        self.requires(self.tested_reference_str)
-
-    def layout(self):
-        # py_version = Version(self.deps_cpp_info["cpython"].version)
-        py_version = Version("3.10.4")
-        cmake_layout(self, src_folder=f"py{py_version.major}")
-
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-
-        tc = CMakeToolchain(self)
-        tc.variables["BUILD_MODULE"] = not is_msvc(self) or self.options["cpython"].shared
-        tc.generate()
-
-        vb = VirtualBuildEnv(self)
-        vb.generate()
-
-        # env = Environment()
-        # env.define("DISTUTILS_USE_SDK", "1")
-        # env.define("MSSdk", "1")
-        # env_vars = env.vars(self, scope = "conanbuild")
-        # env_vars.save_script("setupenv")
-
-        if can_run(self):
-            env = Environment()
-            env.define("DISTUTILS_USE_SDK", "1")
-            env.define("MSSdk", "1")
-
-            if self._cpython_option("with_curses"):
-                env.define("TERM", "ansi")
-            if is_apple_os(self) and not self.options["cpython"].shared:
-                env.append_path("PYTHONPATH", self.build_folder)
-            env_vars = env.vars(self, scope="conanrun")
-            env_vars.save_script("modenv")
-
-            vr = VirtualRunEnv(self)
-            vr.generate()
-
     def build(self):
+        if can_run(self):
+            command = "{} --version".format(self.deps_user_info["cpython"].python)
+            buffer = StringIO()
+            self.run(command, output=buffer, ignore_errors=True, run_environment=True)
+            self.output.info("output: %s" % buffer.getvalue())
+            self.run(command, run_environment=True)
+
         cmake = CMake(self)
-        cmake.configure()
+        py_major = self.deps_cpp_info["cpython"].version.split(".")[0]
+        cmake.definitions["BUILD_MODULE"] = self._supports_modules
+        cmake.definitions["PY_VERSION_MAJOR"] = py_major
+        cmake.definitions["PY_VERSION_MAJOR_MINOR"] = ".".join(self._py_version.split(".")[:2])
+        cmake.definitions["PY_FULL_VERSION"] = self.deps_cpp_info["cpython"].version
+        cmake.definitions["PY_VERSION"] = self._py_version
+        cmake.definitions["PY_VERSION_SUFFIX"] = self._cmake_abi.suffix
+        cmake.definitions["PYTHON_EXECUTABLE"] = self.deps_user_info["cpython"].python
+        cmake.definitions["USE_FINDPYTHON_X".format(py_major)] = self._cmake_try_FindPythonX
+        cmake.definitions["Python{}_EXECUTABLE".format(py_major)] = self.deps_user_info["cpython"].python
+        cmake.definitions["Python{}_ROOT_DIR".format(py_major)] = self.deps_cpp_info["cpython"].rootpath
+        cmake.definitions["Python{}_USE_STATIC_LIBS".format(py_major)] = not self.options["cpython"].shared
+        cmake.definitions["Python{}_FIND_FRAMEWORK".format(py_major)] = "NEVER"
+        cmake.definitions["Python{}_FIND_REGISTRY".format(py_major)] = "NEVER"
+        cmake.definitions["Python{}_FIND_IMPLEMENTATIONS".format(py_major)] = "CPython"
+        cmake.definitions["Python{}_FIND_STRATEGY".format(py_major)] = "LOCATION"
+
+        if is_msvc(self):
+            if Version(self._py_version) < Version("3.8"):
+                cmake.definitions["Python{}_FIND_ABI".format(py_major)] = self._cmake_abi.cmake_arg
+
+        with legacy_tools.environment_append(RunEnvironment(self).vars):
+            cmake.configure()
         cmake.build()
 
-        # if not is_msvc(self) or self.options["cpython"].shared:
-        #     setup_args = [
-        #         self.source_path.joinpath("setup.py"),
-        #         "build",
-        #         "--build-base", self.build_folder,
-        #         "--build-platlib", self.build_path,  #.joinpath("lib_setuptools"),
-        #     ]
-        #     if self.settings.build_type == "Debug":
-        #         setup_args.append("--debug")
-        #     self.run("python {}".format(" ".join("\"{}\"".format(a) for a in setup_args)), env="conanbuild", run_environment=True, cwd=self.source_folder)
+        if can_run(self):
+            if self._supports_modules:
+                with legacy_tools.vcvars(self.settings) if is_msvc(self) else legacy_tools.no_op():
+                    modsrcfolder = "py2" if Version(self.deps_cpp_info["cpython"].version).major < "3" else "py3"
+                    mkdir(self, os.path.join(self.build_folder, modsrcfolder))
+                    for fn in os.listdir(os.path.join(self.source_folder, modsrcfolder)):
+                        shutil.copy(os.path.join(self.source_folder, modsrcfolder, fn), os.path.join(self.build_folder, modsrcfolder, fn))
+                    shutil.copy(os.path.join(self.source_folder, "setup.py"), os.path.join(self.build_folder, "setup.py"))
+                    env = {
+                        "DISTUTILS_USE_SDK": "1",
+                        "MSSdk": "1"
+                    }
+                    env.update(**AutoToolsBuildEnvironment(self).vars)
+                    with legacy_tools.environment_append(env):
+                        setup_args = [
+                            f"{self.source_folder}/setup.py",
+                            # "conan",
+                            # "--install-folder", self.build_folder,
+                            "build",
+                            "--build-base", self.build_folder,
+                            "--build-platlib", os.path.join(self.build_folder, "lib_setuptools"),
+                        ]
+                        if self.settings.build_type == "Debug":
+                            setup_args.append("--debug")
+                        self.run("{} {}".format(self.deps_user_info["cpython"].python, " ".join("\"{}\"".format(a) for a in setup_args)), run_environment=True)
 
     def _test_module(self, module, should_work):
         exception = None
         try:
-            self.run(f"python {self.source_path.parent.joinpath('test_package.py')} -b {self.build_folder} -t {module} ", run_environment=True, env="conanrun")
+            self.run("{} {}/test_package.py -b {} -t {} ".format(
+                self.deps_user_info["cpython"].python, self.source_folder, self.build_folder, module), run_environment=True)
             works = True
         except ConanException as e:
             works = False
@@ -94,25 +146,27 @@ class TestPackageConan(ConanFile):
             self.output.info("Result of test was expected.")
         else:
             if works:
-                raise ConanException(f"Module '{module}' works, but should not have worked")
+                raise ConanException("Module '{}' works, but should not have worked".format(module))
             else:
-                self.output.warn(f"Module '{module}' does not work, but should have worked")
+                self.output.warn("Module '{}' does not work, but should have worked".format(module))
                 raise exception
+
+    def _cpython_option(self, name):
+        try:
+            return getattr(self.options["cpython"], name, False)
+        except ConanException:
+            return False
 
     def test(self):
         if can_run(self):
-            buffer = StringIO()
-            self.run("python -c \"import sys; from pathlib import Path; print(Path(sys.executable).parent)\"", output=buffer, env="conanrun")
-            self.output.info(buffer.getvalue())
-            which_detected = buffer.getvalue().splitlines()[-1].strip()
-            if which_detected not in str(Path(self.deps_user_info["cpython"].python).parent):
-                raise ConanException(f"python reported wrong interpreter. Expected {self.deps_user_info['cpython'].python}. Got {which_detected}.")
+            self.run("{} -c \"print('hello world')\"".format(self.deps_user_info["cpython"].python), run_environment=True)
 
-            self.run("python -c \"import sys; print('.'.join(str(s) for s in sys.version_info[:3]))\"", output=buffer, env="conanrun")
+            buffer = StringIO()
+            self.run("{} -c \"import sys; print('.'.join(str(s) for s in sys.version_info[:3]))\"".format(self.deps_user_info["cpython"].python), run_environment=True, output=buffer)
             self.output.info(buffer.getvalue())
             version_detected = buffer.getvalue().splitlines()[-1].strip()
-            if self.deps_cpp_info["cpython"].version != version_detected:
-                raise ConanException(f"python reported wrong version. Expected {self._py_version}. Got {version_detected}.")
+            if self._py_version != version_detected:
+                raise ConanException("python reported wrong version. Expected {exp}. Got {res}.".format(exp=self._py_version, res=version_detected))
 
             if self._supports_modules:
                 self._test_module("gdbm", self._cpython_option("with_gdbm"))
@@ -120,7 +174,9 @@ class TestPackageConan(ConanFile):
                 self._test_module("bsddb", self._cpython_option("with_bsddb"))
                 self._test_module("lzma", self._cpython_option("with_lzma"))
                 self._test_module("tkinter", self._cpython_option("with_tkinter"))
-                self._test_module("curses", self._cpython_option("with_curses"))
+                with legacy_tools.environment_append({"TERM": "ansi"}):
+                    self._test_module("curses", self._cpython_option("with_curses"))
+
                 self._test_module("expat", True)
                 self._test_module("sqlite3", True)
                 self._test_module("decimal", True)
@@ -129,13 +185,16 @@ class TestPackageConan(ConanFile):
             if is_apple_os(self) and not self.options["cpython"].shared:
                 self.output.info("Not testing the module, because these seem not to work on apple when cpython is built as a static library")
                 # FIXME: find out why cpython on apple does not allow to use modules linked against a static python
-            # else:
-            #     if self._supports_modules:
-            #         self.output.info("Testing module (spam) using cmake built module")
-            #         self._test_module("spam", True)
-            #         self.output.info("Testing module (spam) using cmake built module python_add_library")
-            #         self._test_module("spam2", True)
-            #         self.output.info("Testing module (spam) using setup.py built module")
-            #         self._test_module("spam3", True)
+            else:
+                if self._supports_modules:
+                    with legacy_tools.environment_append({"PYTHONPATH": [os.path.join(self.build_folder, "lib")]}):
+                        self.output.info("Testing module (spam) using cmake built module")
+                        self._test_module("spam", True)
 
-            self.run(self.build_path.joinpath("test_package"), env = "conanrun")
+                    with legacy_tools.environment_append({"PYTHONPATH": [os.path.join(self.build_folder, "lib_setuptools")]}):
+                        self.output.info("Testing module (spam) using setup.py built module")
+                        self._test_module("spam", True)
+
+            # MSVC builds need PYTHONHOME set.
+            with legacy_tools.environment_append({"PYTHONHOME": self.deps_user_info["cpython"].pythonhome}) if self.deps_user_info["cpython"].module_requires_pythonhome == "True" else legacy_tools.no_op():
+                self.run(os.path.join("bin", "test_package"), run_environment=True)
